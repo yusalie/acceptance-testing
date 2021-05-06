7f81d2cd2884a6390dc59896862a4fd06a7a78a6
yusalie <yusuf.salie96@gmail.com>
2021-05-06 14:03:14 +0200

    update

--- templates/tests/acceptance/navigation.feature
+++ templates/tests/acceptance/navigation.feature
@@ -5,3 +5,9 @@ Feature: test navigation between pages
             Given I am on the homepage
              When I click on  the link with id "blog-link"
              Then I am on the blog page
+
+        Scenario: blog can go to the homepage
+            Given I am on the blog page
+             When I click on  the link with id "home-link"
+             Then I am on the homepage
+             
--- templates/tests/acceptance/steps/navigation.py
+++ templates/tests/acceptance/steps/navigation.py
@@ -13,3 +13,13 @@ def step_impl(context):
 def step_impl(contex):
     expected_url = 'http://127.0.0.1:5000/blog'
     assert contex.browser.current_url == expected_url
+    
+@given('I am on the blog page')
+def step_impl(context):
+    browser = webdriver.Chrome('/Users/Yusuf Salie/Downloads/chromedriver_win32 (1)/chromedriver')
+    browser.get('http://127.0.0.1:5000/blog')
+
+@then('I am on homepage')
+def step_impl(contex):
+    expected_url = 'http://127.0.0.1:5000/'
+    assert contex.browser.current_url == expected_url
